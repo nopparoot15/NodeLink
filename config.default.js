3614c82e42e85333df6239890ecfb6fa6b38a904
nopparoot15 <99222124+nopparoot15@users.noreply.github.com>
2026-03-05 07:07:43 +0700

    Update config.default.js
    Signed-off-by: nopparoot15 <99222124+nopparoot15@users.noreply.github.com>

--- config.default.js
+++ config.default.js
@@ -1,7 +1,7 @@
 export default {
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 2333,
     password: 'youshallnotpass',
     useBunServer: false // set to true to use Bun.serve websocket (experimental)
   },
